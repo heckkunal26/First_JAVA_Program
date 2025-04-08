e55d330eb0cb3c3a19df565986ee5a15fd29ea12
Kunal Kharb <kunalkharb45674567@gmail.com>
2025-04-08 22:52:36 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,15 @@
+class hello{
+    public static void main(String a[])
+    {
+      boolean result=true;
+      int x=9;
+      int y=2;
+      if (x>y)
+     System.out.println(x);
+    else
+     System.out.println(y);
+          result = x/y==0?true:false;
+      System.out.println(result);
+
+    }
+}
